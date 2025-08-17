fc05ce495c6e60521872ed15b4c46f1d57a7e23a
Rajesh <45181827+gilgamesh7@users.noreply.github.com>
2025-08-17 06:48:01 +0000

    Using unittest and PyTest

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A generic Python learning notebook to try out snippets from videos and tweets
 
 - To use the marimo notebook : marimo edit python_learning.py
-- The weather.py and tests directory do not use Marimo - confuses pytest. To run tests, use "pytest tests/"
+- The weather.py and tests directory do not use Marimo - confuses pytest. To run tests, use "pytest" - See [How to Write Great Unit Tests in Python](https://youtu.be/EIV_ixKGPmc?si=JX3WigoUmfiy-Ccz)
--- tests/test_weather_patch.py
+++ tests/test_weather_patch.py
@@ -2,6 +2,8 @@
 import pytest
 from typing import Any
 
+from unittest.mock import MagicMock, patch
+
 from weather import WeatherService
 
 def test_get_temperature_with_monkeypatch(monkeypatch: pytest.MonkeyPatch)-> None:
@@ -20,5 +22,51 @@ def test_get_temperature_with_monkeypatch(monkeypatch: pytest.MonkeyPatch)-> Non
     temp = service.get_temperature("Amsterdam")
 
     assert temp==19
-    
+
+def test_get_temperature_with_mock_monkeypatch(monkeypatch: pytest.MonkeyPatch)-> None:
+    def fake_get(url: str, params:dict[str, Any])-> Any:
+        mock_response = MagicMock()
+        mock_response.raise_for_status.return_value = None
+        mock_response.json.return_value = {"current":{"temp_c": 19}}
+
+        return mock_response
+
+
+    monkeypatch.setattr("httpx.get", fake_get)
+
+    service = WeatherService(api_key="fake_key")    
+    temp = service.get_temperature("Amsterdam")
+
+    assert temp==19
+
+def test_get_temperature_with_mock_patch()-> None:
+    mock_response = MagicMock()
+    mock_response.raise_for_status.return_value = None
+    mock_response.json.return_value = {"current":{"temp_c": 19}}
+
+    with patch("httpx.get", return_value=mock_response) as mock_get:
+        service = WeatherService(api_key="fake_key")    
+        temp = service.get_temperature("Amsterdam")
+
+        assert temp == 19
+        mock_get.assert_called_once()
+
+
+@pytest.fixture
+def weather_service(monkeypatch: pytest.MonkeyPatch) -> WeatherService:
+    def fake_get(url: str, params: dict[str, Any]) -> Any:
+        mock_response = MagicMock()
+        mock_response.raise_for_status.return_value = None
+        mock_response.json.return_value = {"current":{"temp_c": 19}}   
+
+        return mock_response
+
+    monkeypatch.setattr("httpx.get", fake_get)  
+    return WeatherService(api_key="fake_key")  
+
+def test_get_temperature_with_fixture(weather_service: WeatherService) -> None:
+    temp = weather_service.get_temperature("Amsterdam")
+    assert temp == 19
+
+
 
