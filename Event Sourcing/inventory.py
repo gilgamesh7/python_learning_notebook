12a2a93c67b807494520e26b4342130597f9125b
Rajesh <45181827+gilgamesh7@users.noreply.github.com>
2025-11-24 08:02:22 +0000

    Add methods to Inventory class to add items

--- Event Sourcing/inventory.py
+++ Event Sourcing/inventory.py
@@ -1,6 +1,8 @@
 from event import Event,EventType
 from event_store import EventStore
 
+from collections import Counter
+
 class Inventory:
     def __init__(self, store: EventStore)-> None :
         self.store = store
@@ -8,3 +10,10 @@ class Inventory:
     def add_item(self,item: str) -> None :
         event = Event(type=EventType.ITEM_ADDED,data=item)
         self.store.append(event)
+
+    def get_items(self) -> list[tuple[str, int]] :
+        counts = Counter[str]()
+        for event in self.store.get_events():
+            if event.type == EventType.ITEM_ADDED:
+                counts[event.data] += 1
+        return list(counts.items())
